09a19c4549fd781ae86477fb0449a6e6437042e6
Ferdy Perdaan <f.perdaan@gmail.com>
2022-08-31 08:09:52 +0200

    Adding CORS headers

--- src/RTLMaze.REST/Program.cs
+++ src/RTLMaze.REST/Program.cs
@@ -1,3 +1,5 @@
+using System.Web.Http.Cors;
+
 var builder = WebApplication.CreateBuilder( args );
 
 // Add services to the container.
@@ -5,11 +7,20 @@ var builder = WebApplication.CreateBuilder( args );
 # region Start configuration
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 
 // -- Configure swagger
 builder.Services.AddSwaggerGen();
+
+// -- Configure cors ( enable all for now )
+builder.Services.AddCors( c => c.AddDefaultPolicy( b => 
+{
+	b.AllowAnyOrigin()
+	 .AllowAnyMethod()
+	 .AllowAnyHeader();
+} ) );
 
 # endregion
 
@@ -17,12 +28,19 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+
+// Configure cors 
+// -- Whitelist all for now ( by default )
+var cors = new EnableCorsAttribute( "*", "*", "*" );
+
 // Configure the HTTP request pipeline.
 
 // -- Add swagger
 app.UseSwagger();
 app.UseSwaggerUI();
 
+// -- Enable cors
+app.UseCors();
 
 app.UseHttpsRedirection();
 //app.UseAuthorization();
